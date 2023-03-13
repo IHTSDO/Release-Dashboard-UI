6e112db2238520f9f6676b0df2578a6eeeaff2e7
Quyen Ly <quocquyenit@gmail.com>
2023-03-13 17:16:30 +0700

    FRI-598 Simplify release build configurations

--- src/app/models/qaConfiguration.ts
+++ src/app/models/qaConfiguration.ts
@@ -4,8 +4,5 @@ export class QAConfiguration {
     enableDrools: boolean;
     enableMRCMValidation: boolean;
     enableTraceabilityValidation: boolean;
-    extensionDependencyRelease: string;
-    previousExtensionRelease: string;
     maxFailureExport: number;
-    previousInternationalRelease: string;
 }
