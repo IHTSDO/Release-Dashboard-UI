e8741d6406baeba3f16f59e565292204aeaded0f
Quyen Ly <quocquyenit@gmail.com>
2025-04-25 15:01:57 +0700

    RAP-143 Improve Error handling failures

--- src/app/components/product-viewer/product-viewer.component.ts
+++ src/app/components/product-viewer/product-viewer.component.ts
@@ -248,12 +248,11 @@ export class ProductViewerComponent implements OnInit, OnDestroy {
             },
             errorResponse => {
                 this.savingProduct = false;
-                this.message = 'Failed to update product. Error: ';
-                if (errorResponse.status >= 500) {
-                    this.message += (errorResponse.error.HTTPStatus.replaceAll('_', ' ')
-                                + '.\nPlease contact technical support to get help resolving this.');
+                if (errorResponse.error && errorResponse.error.errorMessage) {
+                    this.message = errorResponse.error.errorMessage;
                 } else {
-                    this.message += errorResponse.error.errorMessage;
+                    this.message = 'Failed to update product. Error: ' + errorResponse.error.HTTPStatus.replaceAll('_', ' ')
+                    + '.\nPlease contact technical support to get help resolving this.';
                 }
                 this.openErrorModel();
             },
